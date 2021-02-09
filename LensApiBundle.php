d93d56c3a375434e3628d0e90a0344a60343d088
Jenne van der Meer <jennevdmeer@impulze.net>
2021-02-09 14:49:15 +0100

    idk

--- LensApiBundle.php
+++ LensApiBundle.php
@@ -2,13 +2,8 @@
 
 namespace Lens\Bundle\ApiBundle;
 
-use Lens\Bundle\ApiBundle\DependencyInjection\LensApiExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LensApiBundle extends Bundle
 {
-    public function getContainerExtension()
-    {
-        return new LensApiExtension();
-    }
 }
